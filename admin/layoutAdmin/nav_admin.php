eba6b3b500e4b37d6018cc54bc722bfb82cce1e8
wilberbertel <48478044+wilberbertel@users.noreply.github.com>
2020-10-23 23:07:59 -0500

    Add files via upload

---
+++ admin/layoutAdmin/nav_admin.php
@@ -0,0 +1,23 @@
+<div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+<aside class="app-sidebar">
+  <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="Assets/images/avatar.png" alt="User Image">
+    <div>
+      <p class="app-sidebar__user-name">Adrian Atencia</p>
+      <p class="app-sidebar__user-designation">Administrador</p>
+    </div>
+  </div>
+  <ul class="app-menu">
+    <li><a class="app-menu__item" href="dashboard.php"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+    <li><a class="app-menu__item" href="products.php"><i class="app-menu__icon fa fa-archive" aria-hidden="true"></i><span class="app-menu__label">Productos</span></a></li>
+    <li><a class="app-menu__item" href="categories.php"><i class="app-menu__icon fa fa-list-alt" aria-hidden="true"></i><span class="app-menu__label">Categorias</span></a></li>
+    <li><a class="app-menu__item" href=""><i class="app-menu__icon fa fa-shopping-cart" aria-hidden="true"></i><span class="app-menu__label">Pedidos</span></a></li>
+
+    <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-users" aria-hidden="true"></i><span class="app-menu__label"> Configurar usuarios </span><i class="treeview-indicator fa fa-angle-right"></i></a>
+      <ul class="treeview-menu">
+        <li><a class="treeview-item" href="users.php"><i class="icon fa fa-circle-o"></i> Usuarios</a></li>
+        <li><a class="treeview-item" href="config_users.php"><i class="icon fa fa-circle-o"></i> Configurar usuarios</a></li>
+      </ul>
+    </li>
+    <li><a class="app-menu__item" href=""><i class="app-menu__icon fa fa-sign-out" aria-hidden="true"></i><span class="app-menu__label">Logout</span></a></li>
+  </ul>
+</aside>
